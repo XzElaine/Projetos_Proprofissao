284f1fea147ef3e5b80bdb3edeb5842977006a90
Elaine Santos <152093378+XzElaine@users.noreply.github.com>
2024-11-20 13:05:51 -0300

    Update

--- Atividades - Lógica de Programação/Apostila FACCAT - JavaScript/12 - algoritmo - conversão de Fahrenheit em Celsius/js/javascript.js
+++ Atividades - Lógica de Programação/Apostila FACCAT - JavaScript/12 - algoritmo - conversão de Fahrenheit em Celsius/js/javascript.js
@@ -1,3 +1,3 @@
-let fahrenheit = parseFloat(prompt("Digite a temperratura em fahrenheit: "))
+let fahrenheit = parseFloat(prompt("Digite a temperatura em fahrenheit: "))
 let celsius = (5 * (fahrenheit - 32)) / 9
 alert ("O valor da temperatura em Celcius é: " + celsius + "ºC")
