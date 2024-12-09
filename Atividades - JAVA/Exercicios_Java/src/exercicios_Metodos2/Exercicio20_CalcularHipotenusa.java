447263c518da01c999c1ec78ea9c92aec08e2aa9
Elaine Cristina dos Santos <elainesantos.pessoal18@gmail.com>
2024-12-09 02:24:27 -0300

    update

--- Atividades - JAVA/Exercicios_Java/src/exercicios_Metodos2/Exercicio20_CalcularHipotenusa.java
+++ Atividades - JAVA/Exercicios_Java/src/exercicios_Metodos2/Exercicio20_CalcularHipotenusa.java
@@ -1,5 +1,23 @@
 package exercicios_Metodos2;
+import java.util.Scanner;
 
 public class Exercicio20_CalcularHipotenusa {
+    public static double CalcularHipotenusa(double cateto1, double cateto2) {
+        return Math.sqrt(Math.pow(cateto1, 2) + Math.pow(cateto2, 2));
+    }
 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite o valor do primeiro cateto: ");
+        double cateto1 = sc.nextDouble();
+
+        System.out.print("Digite o valor do segundo cateto: ");
+        double cateto2 = sc.nextDouble();
+
+        double hipotenusa = CalcularHipotenusa(cateto1, cateto2);
+        System.out.println("A hipotenusa é: " + hipotenusa);
+
+    }
 }
+
